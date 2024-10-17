8b8903441ef5a173fcfe5db588080f7f080e2f7a
신예지 <71514775+shinyeji28@users.noreply.github.com>
2024-10-17 18:35:40 +0900

    Title: 십진수와 이진수, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

--- 241017/십진수와 이진수/decimal-and-binary-number.java
+++ 241017/십진수와 이진수/decimal-and-binary-number.java
@@ -1,10 +1,11 @@
 import java.util.*;
 import java.io.*;
+import java.math.*;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Long num = Long.parseLong(br.readLine(),2)*17;
-        System.out.println(Long.toBinaryString(num));
-        
+        BigInteger num = new BigInteger(br.readLine(),2);
+        System.out.println(num.multiply(BigInteger.valueOf(17)).toString(2));
+
     }
 }
